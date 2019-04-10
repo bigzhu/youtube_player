ead90360071557e60608dae1f3245763477875be
sarbagyadhaubanjar <49148835+sarbagyadhaubanjar@users.noreply.github.com>
2019-04-10 14:21:16 +0545

    Update player.js

--- player.js
+++ player.js
@@ -1,29 +1,27 @@
-      var player;
-      var timerId;
-      var e = document.createEvent('UIEvent');
-      function onYouTubeIframeAPIReady() {
+var player;
+  	var timerId;
+      	function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
           height: '100%',
-          width: '100%',
+	  width: '100%',
           playerVars: {
             'controls': 0,
-            'autoplay': 1,
-			'enablejsapi': 1,
-			'fs': 0,
-			'origin': 'https://www.youtube.com',
-			'rel': 0,
-			'showinfo': 0,
-			'iv_load_policy': 3,
-			'modestbranding': 1,
-			'cc_load_policy': 0
+            'autoplay': 0,
+	    'enablejsapi': 1,
+	    'fs': 0,
+            'origin': 'https://www.youtube.com',
+	    'rel': 0,
+	    'showinfo': 0,
+	    'iv_load_policy': 3,
+	    'modestbranding': 1,
+	    'cc_load_policy': 0
           },
            events: {
-    				onReady: function(event) { Ready.postMessage("Ready"); event.target.playVideo()},
-    				onStateChange: function(event) { sendPlayerStateChange(event.data) },
-    				onPlaybackQualityChange: function(event) { PlaybackQualityChange.postMessage(event.data) },
-    				onPlaybackRateChange: function(event) { PlaybackRateChange.postMessage(event.data) },
-    				onError: function(error) { Errors.postMessage(error.data) }
-
+    		onReady: function(event) { Ready.postMessage("Ready"); event.target.playVideo()},
+    		onStateChange: function(event) { sendPlayerStateChange(event.data) },
+    		onPlaybackQualityChange: function(event) { PlaybackQualityChange.postMessage(event.data) },
+    		onPlaybackRateChange: function(event) { PlaybackRateChange.postMessage(event.data) },
+    		onError: function(error) { Errors.postMessage(error.data) }
     	},
         });
       }
