a331bf92c0ec02ff8a5697cad87a6f3d2b99bf8a
sarbagyadhaubanjar <49148835+sarbagyadhaubanjar@users.noreply.github.com>
2019-04-21 13:06:23 +0545

    Update player.js

--- player.js
+++ player.js
@@ -9,7 +9,7 @@ var player;
             'autoplay': 0,
 	    'enablejsapi': 1,
 	    'fs': 0,
-            'origin': 'https://www.youtube.com',
+            'origin': 'https://sarbagyadhaubanjar.github.io',
 	    'rel': 0,
 	    'showinfo': 0,
 	    'iv_load_policy': 3,
