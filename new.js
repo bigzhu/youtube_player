8e18977aeeccddae18d6efe269faab789908cc2e
sarbagyadhaubanjar <49148835+sarbagyadhaubanjar@users.noreply.github.com>
2019-05-26 12:36:31 +0545

    Create new.js

---
+++ new.js
@@ -0,0 +1,54 @@
+var player;
+  	var timerId;
+      	function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '100%',
+	  width: '100%',
+    videoId: 'M7lc1UVf-VE',
+          playerVars: {
+            'controls': 0,
+            'autoplay': 0,
+	    'enablejsapi': 1,
+	    'fs': 0,
+            'origin': 'https://www.youtube.com',
+	    'rel': 0,
+	    'showinfo': 0,
+	    'iv_load_policy': 3,
+	    'modestbranding': 1,
+	    'cc_load_policy': 0
+          },
+           events: {
+    		onReady: function(event) {event.target.playVideo(); Ready.postMessage("Ready")},
+    		onStateChange: function(event) { sendPlayerStateChange(event.data) },
+    		onPlaybackQualityChange: function(event) { PlaybackQualityChange.postMessage(event.data) },
+    		onPlaybackRateChange: function(event) { PlaybackRateChange.postMessage(event.data) },
+    		onError: function(error) { Errors.postMessage(error.data) }
+    	},
+        });
+      }
+
+      function sendPlayerStateChange(playerState) {
+            clearTimeout(timerId);
+            StateChange.postMessage(playerState);
+            if(playerState==1){
+                startSendCurrentTimeInterval();
+                sendVideoData(player);
+            }
+      }
+
+      function sendVideoData(player) {
+            var videoData = {
+                'duration'  : player.getDuration(),
+                'videoUrl' : player.getVideoUrl(),
+                'availableQualityLevels' : player.getAvailableQualityLevels(),
+                'videoEmbedCode' : player.getVideoEmbedCode(),
+            };
+            VideoData.postMessage(JSON.stringify(videoData));
+      }
+
+      function startSendCurrentTimeInterval() {
+            timerId = setInterval(function() {
+                CurrentTime.postMessage(player.getCurrentTime());
+                LoadedFraction.postMessage(player.getVideoLoadedFraction());
+            }, 100 );
+      }
