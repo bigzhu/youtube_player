db1797e206b4828f0944bbcf42c7f65ca77ddc65
bigzhu <vermiliondun@gmail.com>
2020-03-17 16:56:13 +0800

    load from local

--- android/player.js
+++ android/player.js
@@ -6,8 +6,8 @@
  */
 var tag = document.createElement("script");
 
-//tag.src = "iframe_api.js";
-tag.src = "https://www.youtube.com/iframe_api";
+tag.src = "iframe_api.js";
+//tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
