d00d2da3a21691f6be0360bd38b2fefb9cd64c17
bigzhu <vermiliondun@gmail.com>
2020-03-19 08:56:08 +0800

    remove load js source from youtube

--- android/player.js
+++ android/player.js
@@ -4,25 +4,6 @@
  * @param  {String} url The URL
  * @return {Object}     The URL parameters
  */
-var tag = document.createElement("script");
-
-//tag.src = "iframe_api.js";
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName("script")[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var getParams = function(url) {
-  var params = {};
-  var parser = document.createElement("a");
-  parser.href = url;
-  var query = parser.search.substring(1);
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    params[pair[0]] = decodeURIComponent(pair[1]);
-  }
-  return params;
-};
 var player;
 var timerId;
 var playerVars = {
